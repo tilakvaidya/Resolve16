d96bdc37e0d8065c4981df9502ec924c93eb7a83
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2015-12-31 18:29:49 +0530

    Added BroadcastReceiver, IntentService skeleton

--- app/src/main/java/com/nvapps/resolve/CreateActivity.java
+++ app/src/main/java/com/nvapps/resolve/CreateActivity.java
@@ -1,6 +1,5 @@
 package com.nvapps.resolve;
 
-import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -11,16 +10,12 @@ import android.widget.Button;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 public class CreateActivity extends AppCompatActivity {
 
     ResolutionsDatabase.ResolutionsDBHelper mDbHelper;
     SQLiteDatabase db;
-    ContentValues values;
-    long newRowId;
     Button startDate;
-    Date d;
     String currentDate;
     Calendar calendar;
     SimpleDateFormat df;
@@ -29,7 +24,10 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         mDbHelper = new ResolutionsDatabase.ResolutionsDBHelper(this);
         db = mDbHelper.getReadableDatabase();
         calendar = Calendar.getInstance();
@@ -37,45 +35,6 @@ public class CreateActivity extends AppCompatActivity {
         currentDate = df.format(calendar.getTime());
         startDate = (Button)findViewById(R.id.button_start_date);
         startDate.setText(currentDate);
-
-       /* values = new ContentValues();
-        values.put(ResolutionsDatabase.ResolutionsEntry.COLUMN_NAME_ENTRY_ID, "1");
-        values.put(ResolutionsDatabase.ResolutionsEntry.COLUMN_NAME_TITLE, "First Resolution");
-        values.put(ResolutionsDatabase.ResolutionsEntry.COLUMN_NAME_CHEAT_COUNTER, "0");
-
-        newRowId = db.insert(
-                ResolutionsDatabase.ResolutionsEntry.TABLE_NAME,
-                null,
-                values);
-        Button button = (Button) findViewById(R.id.show);
-        final TextView textView = (TextView) findViewById(R.id.show_text);
-
-        button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-            db = mDbHelper.getReadableDatabase();
-            String[] projection = {
-                    ResolutionsDatabase.ResolutionsEntry._ID,
-                    ResolutionsDatabase.ResolutionsEntry.COLUMN_NAME_TITLE
-            };
-                    Cursor c = db.query(
-                            ResolutionsDatabase.ResolutionsEntry.TABLE_NAME,  // The table to query
-                            projection,                               // The columns to return
-                            null,                                // The columns for the WHERE clause
-                            null,                            // The values for the WHERE clause
-                            null,                                     // don't group the rows
-                            null,                                     // don't filter by row groups
-                            null                                 // The sort order
-                    );
-                    c.moveToFirst();
-                    String title = c.getString(c.getColumnIndexOrThrow(ResolutionsDatabase.ResolutionsEntry.COLUMN_NAME_TITLE));
-                    textView.setText(title);
-
-
-                }
-            });*/
-
 
     }
 
--- app/src/main/java/com/nvapps/resolve/ResolutionsDatabase.java
+++ app/src/main/java/com/nvapps/resolve/ResolutionsDatabase.java
@@ -5,21 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
-/**
- * Created by Vaidyas on 29-Dec-15.
- */
 public final class ResolutionsDatabase {
-    public ResolutionsDatabase() {
-    }
-
-    public static abstract class ResolutionsEntry implements BaseColumns {
-        public static final String TABLE_NAME = "entry";
-        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
-        public static final String COLUMN_NAME_TITLE = "title";
-        public static final String COLUMN_NAME_CATEGORY = "category";
-        public static final String COLUMN_NAME_CHEAT_COUNTER= "cc";
-    }
-
     private static final String TEXT_TYPE = " TEXT";
     private static final String COMMA_SEP = ", ";
     private static final String SQL_CREATE_ENTRIES =
@@ -30,11 +16,22 @@ public final class ResolutionsDatabase {
                     ResolutionsEntry.COLUMN_NAME_CATEGORY + TEXT_TYPE + COMMA_SEP +
                     ResolutionsEntry.COLUMN_NAME_CHEAT_COUNTER + TEXT_TYPE +
                     ")";
-
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + ResolutionsEntry.TABLE_NAME;
 
+    public ResolutionsDatabase() {
+    }
+
+    public static abstract class ResolutionsEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_CATEGORY = "category";
+        public static final String COLUMN_NAME_CHEAT_COUNTER = "cc";
+    }
+
     public static class ResolutionsDBHelper extends SQLiteOpenHelper {
+
         public static final int DATABASE_VERSION = 1;
         public static final String DATABASE_NAME = "DataReader.db";
 
@@ -44,10 +41,12 @@ public final class ResolutionsDatabase {
         public void onCreate(SQLiteDatabase db) {
             db.execSQL(SQL_CREATE_ENTRIES);
         }
+
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             db.execSQL(SQL_DELETE_ENTRIES);
             onCreate(db);
         }
+
         public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             onUpgrade(db, oldVersion, newVersion);
         }
